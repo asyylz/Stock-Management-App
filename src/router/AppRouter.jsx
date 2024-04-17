1c508d24c254e26fb7bb7dc755dfbe1f58a36be0
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-17 10:00:36 +0100

    nested  layout

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,12 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import BrandsPage from "../pages/Brands";
+import Sales from "../pages/Sales";
+import Purchases from "../pages/Purchases";
+import Firms from "../pages/Firms";
+import Products from "../pages/Products";
 
 const AppRouter = () => {
   return (
@@ -12,7 +18,14 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          {/* Nested routes within the 'stock' route */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="home" element={<Home />} />
+          <Route path="/stock/brands" element={<BrandsPage />} />
+          <Route path="/stock/sales" element={<Sales />} />
+          <Route path="/stock/purchases" element={<Purchases />} />
+          <Route path="/stock/firms" element={<Firms />} />
+          <Route path="/stock/products" element={<Products />} />
         </Route>
       </Routes>
     </Router>
