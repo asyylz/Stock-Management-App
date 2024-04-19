cd33ef9a33fc42c0187ee611455a09f7c78fce64
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-19 10:59:41 +0100

    "Add container, typography, and button components to Firms page; display 'Firms' heading and 'New Firm' button"

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,12 +1,27 @@
 import { useEffect } from 'react';
 import useStockCall from '../hooks/useStockCall';
-
+import { Button, Container, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
 export default function Firms() {
   const { getStockData } = useStockCall();
+  const { firms } = useSelector((state) => state.stock);
+  console.log('firms', firms);
 
   useEffect(() => {
     getStockData('firms');
   }, []);
 
-  return <></>;
+  return (
+    <Container>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h1"
+        color="secondary.second"
+      >
+        Firms
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+    </Container>
+  );
 }
