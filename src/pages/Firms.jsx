f98ca44658c28c52db131c7daea2f556962865fe
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-19 08:38:30 +0100

    firms update

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,12 +1,11 @@
-import { AirplaySharp } from '@mui/icons-material';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
-import { fetchStart } from '../features/stockSlice';
+import { fetchStart, firmsSuccess } from '../features/stockSlice';
 import { useEffect } from 'react';
-
 
 export default function Firms() {
   const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
 
   useEffect(() => {
     getFirms();
@@ -15,8 +14,13 @@ export default function Firms() {
   const getFirms = async () => {
     dispatch(fetchStart());
     try {
-      const data = await axios(`${import.meta.env.BASE_URL}firms`);
+      const { data } = await axios(`${import.meta.env.VITE_BASE_URL}firms`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });
       console.log(data);
+      dispatch(firmsSuccess(data.data));
       return data;
     } catch {}
   };
