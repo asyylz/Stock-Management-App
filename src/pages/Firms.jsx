cffb1e3bec1c0b38f7cbe697bbf9f2130cb9a058
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-19 08:32:38 +0100

    fetch firms

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,5 +1,24 @@
+import { AirplaySharp } from '@mui/icons-material';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import { fetchStart } from '../features/stockSlice';
+import { useEffect } from 'react';
+
+
 export default function Firms() {
-    return (
-        <></>
-    )
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
+  const getFirms = async () => {
+    dispatch(fetchStart());
+    try {
+      const data = await axios(`${import.meta.env.BASE_URL}firms`);
+      console.log(data);
+      return data;
+    } catch {}
+  };
+  return <></>;
 }
