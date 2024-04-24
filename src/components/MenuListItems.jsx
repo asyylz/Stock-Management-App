9485eb83a7508f3ebe105d9a98efc923ab0bed55
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-24 08:24:30 +0100

    Update Products.jsx to include ProductModal and useStockCall; remove unnecessary import in MenuListItems.jsx

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,70 +1,70 @@
-import { Box } from "@mui/material";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import Toolbar from "@mui/material/Toolbar";
-import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Box } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Toolbar from '@mui/material/Toolbar';
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-const icon = (name) => `/assets/navbar/${name}.svg`;
+const icon = (name) => `../../public/navbar/${name}.svg`;
 
 const links = [
   {
-    title: "Dashboard",
-    url: "/stock",
+    title: 'Dashboard',
+    url: '/stock',
     // icon:"/assets/navbar/ic_analytics.svg"
-    icon: icon("ic_analytics"),
+    icon: icon('ic_analytics'),
   },
   {
-    title: "Purchases",
-    url: "/stock/purchases",
-    icon: icon("purchase"),
+    title: 'Purchases',
+    url: '/stock/purchases',
+    icon: icon('purchase'),
   },
   {
-    title: "Sales",
-    icon: icon("sales"),
-    url: "/stock/sales/",
+    title: 'Sales',
+    icon: icon('sales'),
+    url: '/stock/sales/',
   },
   {
-    title: "Firms",
-    icon: icon("firms"),
-    url: "/stock/firms/",
+    title: 'Firms',
+    icon: icon('firms'),
+    url: '/stock/firms/',
   },
   {
-    title: "Brands",
-    icon: icon("brand"),
-    url: "/stock/brands/",
+    title: 'Brands',
+    icon: icon('brand'),
+    url: '/stock/brands/',
   },
   {
-    title: "Products",
-    icon: icon("ic_cart"),
-    url: "/stock/products/",
+    title: 'Products',
+    icon: icon('ic_cart'),
+    url: '/stock/products/',
   },
 ];
 
 const iconStyle = {
-    color: "secondary.main",
-    borderRadius: "1rem",
-    "&:hover": {
-      backgroundColor: "secondary.main",
-      color: "white",
-    },
-  };
-  const selectedStyle = {
-    backgroundColor: "secondary.second",
-    borderRadius: "1rem",
-    "&:hover": {
-      backgroundColor: "secondary.main",
-      color: "secondary.second",
-    },
-    color: "white",
-  };
+  color: 'secondary.main',
+  borderRadius: '1rem',
+  '&:hover': {
+    backgroundColor: 'secondary.main',
+    color: 'white',
+  },
+};
+const selectedStyle = {
+  backgroundColor: 'secondary.second',
+  borderRadius: '1rem',
+  '&:hover': {
+    backgroundColor: 'secondary.main',
+    color: 'secondary.second',
+  },
+  color: 'white',
+};
 
 const MenuListItems = () => {
   const navigate = useNavigate();
-  const {pathname} = useLocation() 
-  console.log(pathname)
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <div>
       <Toolbar />
@@ -73,7 +73,7 @@ const MenuListItems = () => {
           <ListItem key={item.title} disablePadding>
             <ListItemButton
               onClick={() => navigate(item.url)}
-              sx={pathname == item.url ? selectedStyle :iconStyle}
+              sx={pathname == item.url ? selectedStyle : iconStyle}
             >
               {/* <Box
                 sx={{
@@ -92,8 +92,8 @@ const MenuListItems = () => {
                   width: 24,
                   height: 24,
                   mask: `url(${item.icon}) no-repeat center / contain`,
-                  mr:2,
-                  bgcolor: "currentColor",
+                  mr: 2,
+                  bgcolor: 'currentColor',
                 }}
               />
               <ListItemText primary={item.title} />
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,52 @@
-export default function Products() {
-    return (
-        <><div>Products</div></>
-    )
-}
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import ProductModal from '../components/Modals/ProductModal';
+import useStockCall from '../hooks/useStockCall';
+
+const Products = () => {
+  const { getStockData } = useStockCall();
+  const { products } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInitialState({
+      categoryId: '',
+      brandId: '',
+      name: '',
+    });
+  };
+
+  console.log('products:', products);
+  console.log('products:', initialState);
+  useEffect(() => {
+    getStockData('products');
+  }, []);
+
+  return (
+    <Container maxWidth={'xl'}>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h1"
+        color="secondary.second"
+      >
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        New Product
+      </Button>
+      {open && (
+        <ProductModal
+          open={open}
+          handleClose={handleClose}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default Products;
